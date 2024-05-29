24375d8ec9569fa5d0264bfc29c3c486f8ae3e21
yuesli <yuesli@student.ethz.ch>
2024-05-29 16:54:46 +0200

    update function for annimation

--- trajectron/visualization/annimation.py
+++ trajectron/visualization/annimation.py
@@ -1,9 +1,11 @@
 import imageio
 import matplotlib.pyplot as plt
 import os
+import argparse
+
 def create_gif_from_saved_figs(fig_dir: str, t_start: int, t_end: int):
     timesteps = list(range(t_start, t_end))
-    with imageio.get_writer(fig_dir+'pred.gif', mode='I', duration=1.0) as writer:
+    with imageio.get_writer(fig_dir+'/pred.gif', mode='I', duration=1.0) as writer:
         for t in timesteps:
             filename = fig_dir + '/pred_' + str(t) + '.png'
             image = imageio.imread(filename)
@@ -15,7 +17,8 @@ def create_gif_from_saved_figs(fig_dir: str, t_start: int, t_end: int):
 
 
 if __name__ == '__main__':
-    log_dir = '/home/ysli/Desktop/dis-constrained-planning/predictors/Trajectron-plus-plus/experiments/nuScenes/models'
-    model_dir = os.path.join(log_dir, 'robot')
-    output_save_dir = os.path.join(model_dir, 'pred_figs')
-    create_gif_from_saved_figs(output_save_dir+'/scene_0103/stop', t_start=8, t_end=20)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log_dir', type=str, required=True)
+    args = parser.parse_args()
+    # log_dir = '/home/ysli/Desktop/dis-constrained-planning/predictors/Trajectron-plus-plus/experiments/nuScenes/models'
+    create_gif_from_saved_figs(args.log_dir, t_start=8, t_end=20)
